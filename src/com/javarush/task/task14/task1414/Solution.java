8e77f34a9c701d29a3761d5cc577322ad3fee4be
DenisK <35003466+donz99@users.noreply.github.com>
2025-10-22 21:11:46 +0300

    прошёл 6 задач javarush

--- src/com/javarush/task/task14/task1414/Solution.java
+++ src/com/javarush/task/task14/task1414/Solution.java
@@ -2,6 +2,8 @@ package com.javarush.task.task14.task1414;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /* 
@@ -10,12 +12,19 @@ MovieFactory
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        Movie movie=null;
+        HashMap<String, Movie> hashMap = new HashMap<>();
+        hashMap.put("soapOpera",new SoapOpera());
+        hashMap.put("cartoon",new Cartoon());
+        hashMap.put("thriller",new Thriller());
         Scanner sc = new Scanner(System.in);
         while (true){
             String sss=sc.nextLine();
-            if (sss.matches("cartoon|thriller|soapOpera"))
+            movie=
+            MovieFactory.getMovie(sss);
+            if (!hashMap.containsKey(sss))
                 break;
-            Movie movie = new Cartoon|Thriller("");
+            System.out.println(movie.getClass().getSimpleName());
         }
         //ввести с консоли несколько ключей (строк), пункт 7
 
