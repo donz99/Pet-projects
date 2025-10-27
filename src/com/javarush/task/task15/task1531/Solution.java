9c6566b642bbf12e96ee10fe18c258558670c8d2
DenisK <35003466+donz99@users.noreply.github.com>
2025-10-27 22:16:38 +0300

    выполнил 2 задачи javarush

---
+++ src/com/javarush/task/task15/task1531/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task15.task1531;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/* 
+Факториал
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(reader.readLine());
+        reader.close();
+
+        System.out.println(factorial(input));
+    }
+
+    public static String factorial(int n) {
+        BigInteger bg = new BigInteger("1");
+        long sum=1;
+        if (n<0)
+            return "0";
+        else if (n==0)
+            return "1";
+
+
+        else  {
+            for (long i = 1; i < n+1; i++) {
+                bg=bg.multiply(BigInteger.valueOf(i));
+            }
+        return bg+"";
+        }
+        //add your code here
+
+    }
+}
